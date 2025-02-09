2fd3ef95c0a9f12da8f0250d31e7c1ef0a9891ba
Himanshu Shekhar Das <56048544+himanshu3997@users.noreply.github.com>
2025-02-09 17:48:41 +0530

    Update environment.prod.ts

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api/api',
+  apiUrl: 'https://banking-portal-api-hsd797-dev.apps.rm3.7wse.p1.openshiftapps.com',
   tokenName :  "authToken",
   origin:'http://localhost:4200' 
 };
